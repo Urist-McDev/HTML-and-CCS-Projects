5f05ef3fe08f9e049bb29c4a0c489ce94f8ebd92
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-07-05 09:46:31 -0400

    Playing around with VS code

---
+++ Project/Hello_World.js
@@ -0,0 +1,6 @@
+var greeting = 'Hello World!';
+var greeting2 = 'Welcome to Node.js';
+var sentence1 = 'Learning how to debug code with the debugger';
+console.log(greeting);
+console.log(greeting2);
+console.log(sentence1);
